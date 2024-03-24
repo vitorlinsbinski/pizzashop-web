279b239dfb1ac1a487eada0349dfd822ea8b40c9
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-03-24 01:36:35 -0400

    add: sign-up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 
+import { Link } from 'react-router-dom'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -43,6 +45,10 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">

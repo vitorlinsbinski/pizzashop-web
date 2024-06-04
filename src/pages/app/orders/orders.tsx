ccc4326382906882964ea3331e5f2bedd7fceb70
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-06-04 10:08:52 -0400

    add: order pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -11,12 +11,29 @@ import { OrderTableFilters } from './order-table-filters'
 import { Pagination } from '@/components/pagination'
 import { useQuery } from '@tanstack/react-query'
 import { getOrders } from '@/api/get-orders'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((prev) => {
+      prev.set('page', (pageIndex + 1).toString())
+
+      return prev
+    })
+  }
 
   return (
     <>
@@ -52,7 +69,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={104} perPage={10} />
+          {result && (
+            <Pagination
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+              onPageChange={handlePaginate}
+            />
+          )}
         </div>
       </div>
     </>
